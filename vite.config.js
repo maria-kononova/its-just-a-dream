3239984e9f41218052353306313de663fa542863
KononovaM <515nonia515@gmail.com>
2026-04-03 16:54:36 +0300

    е

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/its-just-a-dream/',
+  base: '/its-just-a-dream/', // ВАЖНО: слеши в начале и конце!
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-  },
 });
